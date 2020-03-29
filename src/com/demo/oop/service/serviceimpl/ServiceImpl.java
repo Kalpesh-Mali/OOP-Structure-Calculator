41cf52160a57cfc81050bcb0a589410328cc8e5c
Kalpesh-Mali <50637297+Kalpesh-Mali@users.noreply.github.com>
2020-03-29 05:35:08 +0530

    Update ServiceImpl.java

--- src/com/demo/oop/service/serviceimpl/ServiceImpl.java
+++ src/com/demo/oop/service/serviceimpl/ServiceImpl.java
@@ -3,21 +3,26 @@ package com.demo.oop.service.serviceimpl;
 import com.demo.oop.model.Calci;
 import com.demo.oop.service.CalciService;
 import com.demo.oop.utility.InputScanner;
-
+//Polymorphism
 public class ServiceImpl implements CalciService {
 
+	//Inheritance
+	@Override
 	public void add(Calci sc) {
 		InputScanner.display(sc.getNum1() + sc.getNum2());
 	}
-
+	
+	@Override
 	public void sub(Calci sc) {
 		InputScanner.display(sc.getNum1() - sc.getNum2());
 	}
 
+	@Override
 	public void mul(Calci sc) {
 		InputScanner.display(sc.getNum1() * sc.getNum2());
 	}
 
+	@Override
 	public void div(Calci sc) {
 		InputScanner.display(sc.getNum1() / sc.getNum2());
 	}
